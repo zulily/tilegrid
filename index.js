659d4b2b6eb744d8f645d3d63e8d66e37de8ebd4
Bala Anirudh Kurakula <bkurakula@zulily.com>
2016-05-23 16:31:59 -0700

    Export MultiSelect as a global component

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ var ReactTilegrid = require('./src/tilegridComponent')
 var MultiSelect = require('./src/multiSelect')
 
 if(!(window == null)){
-  window.jQueryTilegrid = jQueryTilegrid;
-  window.ReactTilegrid = ReactTilegrid;
-  window.MultiSelect = MultiSelect;
+  window.jQueryTilegrid = jQueryTilegrid;
+  window.ReactTilegrid = ReactTilegrid;
+  window.MultiSelect = MultiSelect;
 }
 
 module.exports = {
-  jQueryTilegrid:    jQueryTilegrid,
-  ReactTilegrid:     ReactTilegrid,
-  MultiSelect:     MultiSelect
+  jQueryTilegrid:    jQueryTilegrid,
+  ReactTilegrid:     ReactTilegrid,
+  MultiSelect:     MultiSelect
 }
