be8c651fd5e5bd0bc7caabd97e41426962924f8a
Manju <mmanoharan@zulily.com>
2016-06-21 13:22:58 -0700

    Adding MultiSelect as part of index

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-
 var jQueryTilegrid = require('./src/tilegrid')
 var ReactTilegrid = require('./src/tilegridComponent')
+var MultiSelect = require('./src/multiSelect')
 
 if(!(window == null)){
-  window.jQueryTilegrid = jQueryTilegrid;
-  window.ReactTilegrid = ReactTilegrid;
+  window.jQueryTilegrid = jQueryTilegrid;
+  window.ReactTilegrid = ReactTilegrid;
+  window.MultiSelect = MultiSelect;
 }
 
 module.exports = {
-  jQueryTilegrid:    jQueryTilegrid,
-  ReactTilegrid:     ReactTilegrid
+  jQueryTilegrid:    jQueryTilegrid,
+  ReactTilegrid:     ReactTilegrid,
+  MultiSelect:     MultiSelect
 }
-
